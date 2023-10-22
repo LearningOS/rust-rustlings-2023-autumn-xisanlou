78ccaf495f38274203acefcdfe8cb1d7cbdefc19
xisanlou <xisanlou@aliyun.com>
2023-10-22 20:55:15 +0800

    update

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,31 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
+
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+
+        let mut input: Vec<&str> = s.split(",").collect();
+        if input.len() != 2 {
+            return Person::default();
+        }
+
+        let name: String = input[0].to_string();
+        if name == "" {
+            return Person::default();
+        }
+
+        
+        let age: usize = match input[1].parse::<usize>() {
+            Ok(age) => age,
+            Err(_) => return Person::default(),
+        };
+
+        Person{name, age}
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,7 +31,7 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
+
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +52,27 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(ParsePersonError::Empty);
+        };
+
+        let mut input: Vec<&str> = s.split(",").collect();
+        if input.len() != 2 {
+            return Err(ParsePersonError::BadLen);
+        };
+
+        let name: String = input[0].to_string();
+        if name == "" {
+            return Err(ParsePersonError::NoName);
+        };
+
+        
+        let age: usize = match input[1].parse::<usize>() {
+            Ok(age) => age,
+            Err(error) => return Err(ParsePersonError::ParseInt(error)),
+        };
+
+        Ok(Person{name, age})      
     }
 }
 
